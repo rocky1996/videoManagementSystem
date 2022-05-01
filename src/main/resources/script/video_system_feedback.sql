0731c06e41d73dd156a4cff6412b435508444dd9
wujinfan <2791752775@qq.com>
2022-05-01 19:33:20 +0800

    fixed

--- src/main/resources/script/video_system_feedback.sql
+++ src/main/resources/script/video_system_feedback.sql
@@ -7,7 +7,7 @@ CREATE TABLE `video_system_feedback` (
      `user_id` int(10) NOT NULL COMMENT '用户id',
      `feedback_secret_key` text NOT NULL COMMENT '密钥',
      `feedback_time` datetime NOT NULL COMMENT '反馈时间',
-     `is_delete` int(2) NOT NULL COMMENT '物理删除,0:未删除,1:已删除',
+     `is_delete` int(2) NOT NULL COMMENT '逻辑删除,0:未删除,1:已删除',
      `create_time` datetime NOT NULL COMMENT '创建时间',
      `update_time` datetime NOT NULL COMMENT '更新时间',
      PRIMARY key (`id`)
--- src/main/resources/script/video_system_user.sql
+++ src/main/resources/script/video_system_user.sql
@@ -7,7 +7,7 @@ CREATE TABLE `video_system_user` (
     `user_desc` text NOT NULL COMMENT '用户描述',
     `user_status` int(2) NOT NULL COMMENT '是否开启,0:开启,1:关闭',
     `role_id` int(10) NOT NULL COMMENT '角色id',
-    `is_delete` int(2) NOT NULL COMMENT '物理删除,0:未删除,1:已删除',
+    `is_delete` int(2) NOT NULL COMMENT '逻辑删除,0:未删除,1:已删除',
     `create_time` datetime NOT NULL COMMENT '创建时间',
     `update_time` datetime NOT NULL COMMENT '更新时间',
     PRIMARY key (`id`)
