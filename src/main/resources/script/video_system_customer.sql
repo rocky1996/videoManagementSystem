26b50967ed619fd71f5cde60538f72788a160563
wujinfan <2791752775@qq.com>
2022-05-01 12:41:49 +0800

    video_system_customer table

--- src/main/resources/script/video_system_customer.sql
+++ src/main/resources/script/video_system_customer.sql
@@ -1,11 +1,11 @@
-#角色表
-DROM TABLE IF EXISTS `video_system_role`;
-CREATE TABLE `video_system_role` (
+#客户表
+DROM TABLE IF EXISTS `video_system_customer`;
+CREATE TABLE `video_system_customer` (
      `id` int(10) NOT NULL AUTO_INCREMENT COMMIT '主键id',
-     `role_name` varchar(20) NOT NULL COMMIT '角色名称',
-     `role_desc` text NOT NULL COMMIT '角色描述',
+     `customer_name` varchar(20) NOT NULL COMMIT '客户名称',
+     `customer_secret_key` text NOT NULL COMMIT '客户密钥',
      `is_effective` int(2) NOT NULL COMMIT '是否开启,0:开启,1:关闭',
-     `role_having_menu` text NOT NULL COMMIT '菜单权限',
+     `customer_gather_auth` text DEFAULT NULL COMMIT '采集内容配置,以逗号隔开',
      `is_delete` int(2) NOT NULL COMMIT '物理删除,0:未删除,1:已删除',
      `create_time` datetime NOT NULL COMMIT '创建时间',
      `update_time` datetime NOT NULL COMMIT '更新时间',
--- src/main/resources/script/video_system_user.sql
+++ src/main/resources/script/video_system_user.sql
@@ -7,6 +7,7 @@ CREATE TABLE `video_system_user` (
     `user_desc` text NOT NULL COMMIT '用户描述',
     `is_effective` int(2) NOT NULL COMMIT '是否开启,0:开启,1:关闭',
     `role_id` int(10) NOT NULL COMMIT '角色id',
+    `is_delete` int(2) NOT NULL COMMIT '物理删除,0:未删除,1:已删除',
     `create_time` datetime NOT NULL COMMIT '创建时间',
     `update_time` datetime NOT NULL COMMIT '更新时间',
     PRIMARY key (`id`)
