26276f1b528fb0b639b6757dcac5f8af2faaf132
wujinfan <2791752775@qq.com>
2022-05-10 20:54:17 +0800

    update

--- src/main/resources/script/video_system_auto_rules.sql
+++ src/main/resources/script/video_system_auto_rules.sql
@@ -2,11 +2,12 @@
 DROP TABLE IF EXISTS `video_system_auto_rules`;
 CREATE TABLE `video_system_auto_rules` (
      `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-     `username` varchar(20) NOT NULL COMMENT '用户名',
-     `password` varchar(20) NOT NULL COMMENT '密码',
---     `user_desc` text NOT NULL COMMENT '用户描述',   #字段删除
-     `user_status` int(2) NOT NULL COMMENT '是否开启,0:开启,1:关闭',
-     `role_id` int(10) NOT NULL COMMENT '角色id',
+     `rule_name` varchar(20) NOT NULL COMMENT '策略名称',
+     `language_type` int(2) NOT NULL COMMENT '语种',
+     `auditor` text NOT NULL COMMENT '审核员id,用逗号隔开',
+     `allocation_rules` text NOT NULL COMMENT '审核员分配的权重,json字符串',
+     `role_status` int(2) NOT NULL COMMENT '是否开启,0:开启,1:关闭',
+     `role_desc` text DEFAULT NULL COMMENT '描述',
      `is_delete` int(2) NOT NULL COMMENT '逻辑删除,0:未删除,1:已删除',
      `create_time` datetime NOT NULL COMMENT '创建时间',
      `update_time` datetime NOT NULL COMMENT '更新时间',
