f2f2cf1c19617905eb4778e239539cad4fd13c59
wujinfan <2791752775@qq.com>
2022-06-14 15:27:47 +0800

    test

--- src/main/java/com/example/videomanagementsystem/util/ExecuteCmdUtil.java
+++ src/main/java/com/example/videomanagementsystem/util/ExecuteCmdUtil.java
@@ -1,35 +1,37 @@
 package com.example.videomanagementsystem.util;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.omg.CORBA.INTERNAL;
 
+import javax.print.DocFlavor;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class ExecuteCmdUtil {
-    public static String executeCmdCommand(String cmd) {
-        if (StringUtils.isNotBlank(cmd)) {
-            StringBuffer sBuffer = new StringBuffer();
-            try {
-                Process process = Runtime.getRuntime().exec(cmd);
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(process.getInputStream(), "GBK"));
-
-                String strLine = null;
-                while ((strLine = bufferedReader.readLine()) != null) {
-                    sBuffer.append(strLine).append(" ");
-                }
-                return sBuffer.toString();
-            } catch (Exception e) {
-                log.error("ExecuteCmdUtil.executeCmdCommand has error",e.getMessage());
-            }
+    public static Integer executeCmdCommand(String cmd) throws Exception{
+        try {
+            List<String> commands = Lists.newArrayList();
+            commands.add("cmd.exe");
+            commands.add("/c");
+            commands.add(cmd);
+            ProcessBuilder builder = new ProcessBuilder(commands);
+            Process process = builder.start();
+            return process.waitFor();  // status为0，成功
+        }catch (IOException e) {
+            log.error("ExecuteCmdUtil.executeCmdCommand has error",e.getMessage());
+            return null;
         }
-        return null;
     }
 
-    public static void main(String[] args) {
-        String str = executeCmdCommand("java -version");
-        log.info("str:{}",str);
-    }
+//    public static void main(String[] args) throws Exception{
+//        Integer result = executeCmdCommand("java -version");
+//        log.info("result:{}",result);
+//    }
 }
